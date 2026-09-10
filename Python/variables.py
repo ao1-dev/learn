0e70628d75e587fb9bc3fb4b1a6566d4353df6ca
aodev89 <exalcius@gmail.com>
2026-09-10 18:07:44 +0200

    update

--- Python/variables.py
+++ Python/variables.py
@@ -1,12 +1,2 @@
 first_code = "Hello world!"
 print(first_code)
-
-def user(self, name, age):
-    name = self.name,
-    age = self.age
-    return name, age
-
-na = user(alx, 30)
-
-print(na)
-    
