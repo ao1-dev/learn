e6c80c7208ab7b67c8fe04127497134b5e8bb9ff
aodev89 <exalcius@gmail.com>
2026-09-10 18:05:15 +0200

    update

--- Python/variables.py
+++ Python/variables.py
@@ -1,4 +1,12 @@
-# Variabler: En variabel er en container, der kan gemme en værdi.
-
 first_code = "Hello world!"
 print(first_code)
+
+def user(self, name, age):
+    name = self.name,
+    age = self.age
+    return name, age
+
+na = user(alx, 30)
+
+print(na)
+    
